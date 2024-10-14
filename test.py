a77ebd458a9a536091213ef723df5d2fbba86c54
HasibAlam <hasibalamsadat2001@gmail.com>
2024-10-14 21:25:19 +1100

    Initial commit with simple Python app and tests

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from main import add
+
+class TestApp(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+if __name__ == "__main__":
+    unittest.main()
